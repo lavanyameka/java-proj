1007e29f1de5cd9a4a4dd08795209dfa82b5d472
Uday Kiran <Uday Kiran>
2017-07-06 17:14:49 +0530

    saving

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("first pass");
+System.out.println("second pass");
+System.out.println("Third pass");
+
+	}
+
+}
